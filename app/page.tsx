89b328be5d7f8571c8235fa195b8b5144fb83acf
ApurbSinha77 <aksinha160368@gmail.com>
2024-02-06 23:28:12 +0530

    mera kaam

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Hero from '@/components/main/Hero'
+import Skills from '@/components/main/Skills'
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className='h-full w-full relative'>
       <div className="flex flex-col h-[850px]">
-      <h1 className='text-slate-400'>Home Portfolio</h1>
       <Hero/>
+      <Skills/>
       </div>
       
     </main>
